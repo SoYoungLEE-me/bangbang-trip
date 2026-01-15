ae28e5f70bf92ecc6f45db4a92cc05b28849472a
JiHyoung You <fbwlgud51@gmail.com>
2026-01-15 18:51:26 +0900

    fix: proxy 해결

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // 이제 /api/tour/areaBasedList2 라고 호출하면
+      // https://apis.data.go.kr/B551011/KorService2/areaBasedList2 로 연결됩니다.
+      "/api/tour": {
+        target: "https://apis.data.go.kr/B551011/KorService2",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/tour/, ""), // /api/tour 부분만 제거
+      },
+    },
+  },
 });

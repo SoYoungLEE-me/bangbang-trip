2ed75e38b5daa7ff542f11b740396d411ff679a0
SoYoungLEE-me <lsy000626@gmail.com>
2026-01-16 10:56:06 +0900

    fix: PublicRouter 다시 수정

--- src/routes/PublicRouter.tsx
+++ src/routes/PublicRouter.tsx
@@ -5,7 +5,7 @@ const PublicRouter = () => {
   const { user } = useAuthStore();
 
   if (user) {
-    return <Navigate to="/login" replace />;
+    return <Navigate to="/" replace />;
   }
 
   return <Outlet />;

fb6bcf26b94292fa0051c48521e5cb5ffe122bcc
JiHyoung You <fbwlgud51@gmail.com>
2026-01-17 15:40:57 +0900

    feat: 로딩 스피너 너비값 수정

--- src/common/components/LoadingSpinner.tsx
+++ src/common/components/LoadingSpinner.tsx
@@ -9,11 +9,12 @@ const LoadingSpinner = ({ size = 40, thickness = 4 }: SpinnerProps) => {
   return (
     <Box
       sx={{
-        width: "100vw",
+        width: "100%",
         height: "100vh",
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
+        overflow: "hidden",
       }}
     >
       <CircularProgress color="primary" size={size} thickness={thickness} />
